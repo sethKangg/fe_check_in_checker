f2c5dc999e2dd637e6721fb448c67be52fa79bf2
SethKangg <maybead10x@gmail.com>
2023-02-26 16:30:20 +0700

    fix bug import store.js redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducer from './reducer/rootReducer';
 
@@ -10,7 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
 let persistor = persistStore(store);
 
 export { store, persistor };
